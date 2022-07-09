83d99999d63cc8ad8d27f556d73fac64581dc52b
Harshil <harshil.chordia@gmail.com>
2022-07-09 13:02:27 +0100

    layout probably working

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,27 +16,32 @@ const Home = () => {
         },4000)
     }, [])
     return (
-        <div className='container home-page'>
-            <div className='text-zone'>
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
+        <div className='container'>
+            <div className='home-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
 
 
-                <img src={LogoH} alt="logo"/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                index={15}/>
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                index={22}/>
-               </h1>
-                <h2> Frontend / Backend / Everything</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                        <img src={LogoH} alt="logo"/>
+                        <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        index={15}/>
+                        <br />
+                        <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        index={22}/>
+                    </h1>
+                    <h2> Frontend / Backend / Everything</h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
+            </div>
+            <div className='navig'>
+                hello
 
             </div>
         </div>
